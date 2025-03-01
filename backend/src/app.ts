b658c7218562bd95d4c607dcff7d99245de62deb
Nga Vu <134727438+zlnga1001@users.noreply.github.com>
2025-03-01 12:45:36 -0500

    folder added

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+    
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
